b1fa41b676d13ea16c48f1ed90965690412d24a5
sheth-jay <jay.sheth@simformsolutions.com>
2023-07-02 13:22:42 +0530

    complete SSR setup

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Link, Route, Routes } from "react-router-dom";
+import About from "./About";
+import Home from "./Home";
+
+const App = () => {
+  return (
+    <div className="app">
+      <h1>Hello There !</h1>
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
